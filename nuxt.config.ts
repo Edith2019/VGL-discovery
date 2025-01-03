27d0c475e36c1677cd2d7fb981e2acdf412fcb1d
Edith <edith.chevallier@hotmail.fr>
2025-01-03 17:57:42 +0100

    add layout and algolia test req

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+    compatibilityDate: "2024-11-01",
+    ssr: true,
+    devtools: { enabled: true },
+    modules: ["@nuxtjs/algolia"],
+    algolia: {
+        apiKey: process.env.ALGOLIA_SEARCH_API_KEY,
+        applicationId: process.env.ALGOLIA_APPLICATION_ID,
+        instantSearch: {
+            theme: "algolia",
+        },
+    },
+});
---
+++ pages/search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>search comp</div>
+</template>
+
+<script lang="ts" setup>
+console.log("search");
+
+const { result, search } = useAlgoliaSearch("movie");
+onMounted(async () => {
+    await search({ query: "Jurassic" });
+});
+</script>
+
+<style></style>
